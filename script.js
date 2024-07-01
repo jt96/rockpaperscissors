f0c4ad99d71a4c1a00336d1b83f77220e7c6f58d
Jackie Trenh <29517154+jt96@users.noreply.github.com>
2024-06-30 17:52:10 -0700

    add comp and human variables

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function getComputerChoice() {
-    return Math.random(3)
+    let numChoice = Math.floor(Math.random() * 3) + 1
+    if (numChoice == 1) {
+        return "rock"
+    } else if (numChoice == 2) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
 }
 
-console.log(getComputerChoice())
+function getHumanChoice() {
+    let humanChoice = prompt("rock, paper, or scissors?")
+    return humanChoice
+}
+
+let compChoice = getComputerChoice()
+let humanChoice = getHumanChoice()
+
