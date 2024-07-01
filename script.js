a84d6ec08ad4c0d40fc1132947f8981c74d1ccfb
Jackie Trenh <29517154+jt96@users.noreply.github.com>
2024-06-30 18:27:36 -0700

    add play round function and loop to play 5 rounds

--- script.js
+++ script.js
@@ -14,8 +14,47 @@ function getHumanChoice() {
     return humanChoice
 }
 
-let compChoice = getComputerChoice()
-let humanChoice = getHumanChoice()
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase()
+    computerChoice = computerChoice.toLowerCase()
+    if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log("You lose! Paper beats Rock")
+        computerScore += 1
+    } else if (humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("You win! Rock beats Scissors")
+        humanScore += 1
+    } else if (humanChoice == "rock" && computerChoice == "rock") {
+        console.log("You tie! Rock is also Rock")
+    } else if (humanChoice == "paper" && computerChoice == "paper") {
+        console.log("You tie! Paper is also Paper")
+    } else if (humanChoice == "paper" && computerChoice == "scissors") {
+        console.log("You lose! Scissors beat Paper")
+        computerScore += 1
+    } else if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log("You win! Paper beats Rock")
+        humanScore += 1
+    } else if (humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("You win! Scissors beat Paper")
+        humanScore += 1
+    } else if (humanChoice == "scissors" && computerChoice == "scissors") {
+        console.log("You tie! Scissors is also Scissors")
+    } else if (humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("You lose! Rock beats Scissors")
+        computerScore += 1
+    }
+}
 
 let computerScore = 0
 let humanScore = 0
+
+for (let i = 1; i <= 5; i++) {
+    console.log("ROUND " + i + " BEGIN!!!")
+    let computerSelection = getComputerChoice()
+    let humanSelection = getHumanChoice()
+
+    console.log("computer selected " + computerSelection)
+    console.log("human selected " + humanSelection)
+    playRound(humanSelection, computerSelection)
+    console.log("human: " + humanScore)
+    console.log("computer: " + computerScore)
+}
